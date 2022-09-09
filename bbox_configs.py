cc6719f0b28e81d806f312a06b734b816e1ee6b0
fksato <f.kazuo.sato@gmail.com>
2022-09-09 13:38:22 -0500

    add bbox config

---
+++ bbox_configs.py
@@ -0,0 +1,9 @@
+categories = [
+    {
+        "id": "0",
+        "name": "Mouth Bounding Box",
+        "supercategory": "human",
+        "keypoints": [],
+        "keypoints_style": []
+    }
+]
